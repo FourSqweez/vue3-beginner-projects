a4a3c4289fa29d357f83b11dd28528dac333a203
Foursqweez <jeerasak.code@gmail.com>
2025-03-15 18:37:10 +0700

    Validate form

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -1,9 +1,11 @@
 <template>
-  <form>
+  <form @submit.prevent="handleSubmit">
     <label>Email:</label>
     <input type="email" required v-model="email" />
+
     <label>Password:</label>
     <input type="password" required v-model="password" />
+    <div v-if="passwordError" class="error">{{ passwordError }}</div>
 
     <label>Role:</label>
     <select v-model="role">
@@ -21,6 +23,9 @@
       <input type="checkbox" required v-model="terms" />
       <label>Accept terms and conditions</label>
     </div>
+    <div class="submit">
+      <button>Create an Account</button>
+    </div>
   </form>
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
@@ -32,18 +37,17 @@
 export default {
   data() {
     return {
-      email: 'test',
+      email: '',
       password: '',
       role: 'designer',
       terms: false,
       tempSkill: '',
       skills: [],
+      passwordError: '',
     }
   },
   methods: {
     addSkill(e) {
-      console.log(e)
-
       if (e.key === ',' && this.tempSkill) {
         if (!this.skills.includes(this.tempSkill)) {
           this.skills.push(this.tempSkill)
@@ -53,6 +57,19 @@ export default {
     },
     deleteSkill(skill) {
       this.skills = this.skills.filter((item) => item !== skill)
+    },
+    handleSubmit() {
+      //   validate password
+      this.passwordError =
+        this.password.length > 5 ? '' : 'Password must be at least 6 chars long'
+
+      if (!this.passwordError) {
+        console.log('email :', this.email)
+        console.log('password :', this.password)
+        console.log('role :', this.role)
+        console.log('skills :', this.skills)
+        console.log('terms accepted :', this.terms)
+      }
     },
   },
 }
@@ -104,5 +121,22 @@ input[type='checkbox'] {
   font-weight: bold;
   color: #777;
   cursor: pointer;
+}
+button {
+  background: #0b6dff;
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 20px;
+  color: white;
+  border-radius: 20px;
+}
+.submit {
+  text-align: center;
+}
+.error {
+  color: red;
+  margin-top: 10px;
+  font-size: 0.8em;
+  font-weight: bold;
 }
 </style>
