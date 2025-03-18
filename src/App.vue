3249bda0cf432d798a61ad3b42d67c169559962c
Foursqweez <jeerasak.code@gmail.com>
2025-03-18 11:32:35 +0700

    Template ref

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <p ref="p">My name is {{ name }} and I am {{ age }} years old</p>
+    <button @click="handleClick">Click me</button>
   </div>
   <router-view />
 </template>
 
 <script>
+import { ref } from 'vue'
+
 export default {
   name: 'Home',
-  setup() {},
+  setup() {
+    let name = 'mario'
+    let age = 30
+
+    const p = ref(null)
+
+    const handleClick = () => {
+      console.log(p, p.value)
+      p.value.classList.add('test')
+      p.value.textContent = 'hello world'
+    }
+
+    return { name, age, handleClick, p }
+  },
 }
 </script>
 
@@ -19,5 +36,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.test {
+  color: red;
 }
 </style>
